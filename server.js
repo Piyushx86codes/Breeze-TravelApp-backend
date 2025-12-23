0e49812f54873d77ed17c449de4bdf2ff8ff06d9
Piyush Kamlakar <you@example.com>
2025-12-23 09:12:16 +0530

    db connectivity setup done and hotelschema done

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+
 const hotelRouter = require("./routes/hotel.router");
+const connectDb = require("./config/dbconfig")
 
-const PORT  = 3500;
+const PORT = 3500;
 app.use(express.json());
+connectDb();
 
-app.get("/",(req,res)=>{
-    return "Hello World";
+app.get("/", (req, res) => {
+    res.send("Hello World!")
 })
 
-app.use("/api/hotels",hotelRouter);
+app.use("/api/hotels", hotelRouter);
 
-app.listen(process.env.PORT || PORT,()=>{
-    console.log(`Server is up on Port No ${PORT}`)
+mongoose.connection.once("open",() => {
+    console.log("DB Connection Successfull");
+    app.listen(process.env.PORT || PORT, () => {
+        console.log(`Server is up on Port No ${PORT}`)
+    })
 })
+
