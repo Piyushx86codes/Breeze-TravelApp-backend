2408c272b74109c803a4f12ebe511a34fc5147a1
Piyush Kamlakar <you@example.com>
2025-12-30 08:30:40 +0530

    jwt token bug resolved

--- routes/wishlist.router.js
+++ routes/wishlist.router.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const Wishlist = require("../model/wishlist.model");
 const router = express.Router();
+const verifyUser = require("../middleware/verifyUser");
 
-router.route("/").post(async(req,res)=>{
+router.route("/").post(verifyUser,async(req,res)=>{
     const newWishlist = new Wishlist(req.body);
     try {
-        const savedWishlist = await Wishlist.save();
+        const savedWishlist = await newWishlist.save();
         res.status(201).json(savedWishlist);
     } catch (error) {
         res.status(500).json({messsage:"failed to create Wishlist"})
     }
 })
 
-router.route("/:id").delete(async(req,res)=>{
+router.route("/:id").delete(verifyUser,async(req,res)=>{
     try {
        await Wishlist.findByIdAndDelete(req.params.id); 
        res.json({message:"Hotel deleted from the Wishlist"});
@@ -21,7 +22,7 @@ router.route("/:id").delete(async(req,res)=>{
     }
 })
 
-router.route("/").get(async(req,res)=>{
+router.route("/").get(verifyUser,async(req,res)=>{
      try {
         const wishlist = await wishlist.find({});
         wishlist ? res.json(wishlist) : res.json({message:"No items found in the Wishlist"});
